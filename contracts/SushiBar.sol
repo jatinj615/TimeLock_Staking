f5ca8b187a0bd2540ddeb1da4855e63290505ca3
Jatin Jain <jatinj615@gmail.com>
2022-07-30 15:27:45 +0530

    feat: basic redeem with tax and unlock period

--- contracts/SushiBar.sol
+++ contracts/SushiBar.sol
@@ -10,15 +10,16 @@ contract SushiBar is ERC20{
     using SafeMath for uint256;
     IERC20 public sushi;
 
-    uint256 intervalDuration; // unstaking interval duration 
+    uint256 intervalUnlockDuration; // unstaking interval duration 
     uint256 intervals; // number of intervals to unstake
     
     uint256 totalAmountStaked;
 
     struct UserStake {
-        uint256 startTimestamp;
-        uint256 amount;
-        address user;
+        uint256 startTimestamp; // staking start timestamp
+        uint256 initialPricePerShare;
+        uint256 shares;
+        address user;   // user
     }
 
 
@@ -39,6 +40,7 @@ contract SushiBar is ERC20{
             userStakings[stakeId] = UserStake(
                 block.timestamp,
                 _amount,
+                1,
                 msg.sender
             );
             _mint(msg.sender, _amount);
@@ -47,22 +49,70 @@ contract SushiBar is ERC20{
             userStakings[stakeId] = UserStake(
                 block.timestamp,
                 what,
+                _amount.div(what),
                 msg.sender
             );
             _mint(msg.sender, what);
         }
+        userStakings[stakeId] = UserStake(
+                block.timestamp,
+                _amount,
+                _amount.div(what),
+                msg.sender
+            );
         ++userStakingCount[msg.sender];
         sushi.transferFrom(msg.sender, address(this), _amount);
     }
 
     // Leave the bar. Claim back your SUSHIs.
-    function leave(uint256 _share) public {
+    function leave(uint256 _share, bytes32 stakingId) public {
+        UserStake memory stake = userStakings[stakingId];
+        require(block.timestamp - stake.startTimestamp > intervalUnlockDuration, "ERR_LOCKED_STAKING");
+        // calculate how much user can withdraw
+        uint256 unstakeLimit = getUnstakeLimitForStake(stake);
+        require(_share <= unstakeLimit, "ERR_SHARES_LOCKED");
+        uint256 what = getUnderlyingForShareAfterTax(_share, stake);
         uint256 totalShares = totalSupply();
-        uint256 what = _share.mul(sushi.balanceOf(address(this))).div(totalShares);
+        uint256 pricePerShare = sushi.balanceOf(address(this)).div(totalShares);
+        // get rewards
+        uint256 rewards = _share.mul(pricePerShare - stake.initialPricePerShare);
+        // calculate tax on rewards
+        uint256 taxOnRewards = getTaxOnRewards(stake, rewards);
+        uint256 what = (_share.mul(sushi.balanceOf(address(this))).div(totalShares)).sub(taxOnRewards);
+        userStakings.shares -= _share;
         _burn(msg.sender, _share);
         sushi.transfer(msg.sender, what);
     }
 
+    function getTaxOnRewards(UserStake memory _stake, uint256 _reward) public view returns (uint256) {
+        uint256 redeemCycle = ((block.timestamp).sub(_stake.startTimestamp)).div(intervalUnlockDuration);
+        if(redeemCycle > intervals) {
+            return _reward;
+        }
+        return (_reward.mul(interval - redeemCycle)).div(intervals);
+    }
+
+    function getUnstakeLimitForStake(UserStake memory _stake) public view returns (uint256) {
+        uint256 redeemCycle = ((block.timestamp).sub(_stake.startTimestamp)).div(intervalUnlockDuration);
+        if(redeemCycle > intervals) {
+            return balanceOf(msg.sender);
+        }
+        return (balanceOf(msg.sender).mul(redeemCycle)).div(intervals);
+    }
+
+    function getUnderlyingForShareAfterTax(uint256 _share, UserStake memory _stake) public view returns (uint256) {
+        uint256 totalShares = totalSupply();
+        uint256 totalShares = balanceOf(msg.sender);
+        uint256 redeemCycle = ((block.timestamp).sub(_stake.startTimestamp)).div(intervalUnlockDuration);
+        uint256 shareLimit =  (balanceOf(msg.sender).mul(redeemCycle)).div(intervals);
+        require(shareLimit > _share, "ERR_INSUFFICIENT_SHARES_UNLOCKED");
+        uint256 initialShareBalance = _stake.amount.div(_share);
+        uint256 rewardsOnShares = _share
+    }
+
+    function getTaxLimit() public view returns (uint256) {
+
+    }
 
     function getStakeIdForUser(address _user) public view returns (bytes32) {
         return getStakeIdForUserAtIndex(_user, userStakingCount[_user]);
